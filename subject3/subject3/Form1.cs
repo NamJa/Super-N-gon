51c1d2202bd409d839580222f95411cc7260dbda
JongWooKim <kjwoo810@gmail.com>
2017-05-24 05:48:24 +0900

    print coordinates of small triangle

--- subject3/subject3/Form1.cs
+++ subject3/subject3/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace subject3
 {
@@ -27,9 +28,12 @@ namespace subject3
             int height = ClientSize.Height;
             int width = ClientSize.Width;
 
-            //g.FillPolygon(new SolidBrush(Color.FromArgb(188, 255, 100)), nPoints2);
-            DrawPolygon(g, pen, this.ClientSize.Width / 2, this.Height / 2, 100, 60, 360/n);
+            
+            DrawPolygon(g, pen, this.ClientSize.Width / 2, this.Height / 2, 200, 60, 360/n);
+            DrawPlygon(g, pen, this.ClientSize.Width / 2, this.Height / 2, 100, 60, 360/n);
         }
+
+
 
         private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
@@ -61,6 +65,25 @@ namespace subject3
                     break;
             }
         }
+        public void DrawPlygon(Graphics graphics, Pen pen, int originX, int originY, double radius, double startDegree, double intervalDegree)
+        {
+            int polygonCount = Convert.ToInt32(360d / intervalDegree);
+
+            List<Point> pointList = new List<Point>();
+
+            for (double i = startDegree; i < startDegree + intervalDegree * polygonCount; i += intervalDegree)
+            {
+                pointList.Add(GetCirclePoint(originX, originY, radius, i));
+            }
+            
+            graphics.FillPolygon(new SolidBrush(Color.FromArgb(0, 200, 136)),  pointList.ToArray());
+            for(int i = 0; i < n; i++)
+            {
+                MessageBox.Show(pointList[i].ToString());
+            }
+        }
+
+
         public void DrawPolygon(Graphics graphics, Pen pen, int originX, int originY, double radius, double startDegree, double intervalDegree)
         {
             int polygonCount = Convert.ToInt32(360d / intervalDegree);
